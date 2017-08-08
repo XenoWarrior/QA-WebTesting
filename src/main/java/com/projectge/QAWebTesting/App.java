f877fe3e70aa58f427dff70142a8364908d43d1b
Ashley Scott <admin@projectge.com>
2017-08-08 09:19:19 +0000

    Initial commit
    Maven project

---
+++ src/main/java/com/projectge/QAWebTesting/App.java
@@ -0,0 +1,13 @@
+package com.projectge.QAWebTesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
